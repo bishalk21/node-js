c9f484bd329066594d974e426d549c6e2a0a4b15
bishal-codes <0karkibishal@gmail.com>
2024-09-26 02:52:51 +1000

    feed api and pagination

--- src/routers/test-user/userRouter.js
+++ src/routers/test-user/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authAdmin } = require("../../middlewares/auth");
 const { ConnectionReqModel } = require("../../models/connection/connectionReq");
+const { testUserModel } = require("../../models/user/user");
 const router = express.Router();
 
 const USER_SAFE_DATA = "firstName lastName age gender photoUrl about skills";
@@ -49,6 +50,68 @@ router.get("/connections", authAdmin, async (req, res, next) => {
     });
   } catch (error) {
     res.statusCode(400).send("Error: " + error.message);
+  }
+});
+
+// feed api
+router.get("/feed?page=1&limit=100", authAdmin, async (req, res, next) => {
+  try {
+    // user [Bishal] should see all the other users [Anil, Suman, Akshay, Arjun, Keshav] except
+    // - himself,
+    // - and the users who are connected with him (if Bishal is connected with Anil, he should not see Anil in the feed, but [Suman, Akshay, Arjun, Keshav] should be visible)
+    // - and Anil should not see Bishal in the feed
+    // - and the users who have sent him connection request (if Suman has sent Bishal a connection request, Bishal should not see Suman in the feed, but [ Akshay, Arjun, Keshav] should be visible)
+    // - and Bishal should not see the users to whom he has sent connection request
+    // if Bishal has rejected the connection request from Anil, he should not see Anil in the feed
+    // Anil should not see Bishal in the feed
+    // user should see all the users who are not connected with him and have not sent him connection request [Akshay, Arjun, Keshav]
+    const loggedInUserId = req.user._id;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    limit = limit > 100 ? 100 : limit;
+
+    // connection requests that are sent from and received by the logged in user
+    const connectionRequests = await ConnectionReqModel.find({
+      $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+    }).select("fromUserId toUserId");
+    // .populate("fromUserId", "firstName")
+    // .populate("toUserId", "firstName");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    console.log(hideUsersFromFeed);
+
+    const users = await testUserModel
+      .find({
+        $and: [
+          {
+            _id: {
+              $nin: Array.from(hideUsersFromFeed),
+            },
+          },
+          {
+            _id: {
+              $ne: loggedInUserId,
+            },
+          },
+        ],
+      })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send({
+      message: "Feed fetched successfully",
+      users,
+    });
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
   }
 });
 
